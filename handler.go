4278ec37ac77e9ee5dd1b17f4d496cb23ba58cc2
Yuri Andri Gani <yuri@inovasiriset.co.id>
2021-12-27 10:09:52 +0700

    first commit

---
+++ handler.go
@@ -0,0 +1,114 @@
+package oauth2fiber
+
+import (
+	// "net/http"
+	"encoding/base64"
+	"strings"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/gofiber/fiber/v2"
+)
+
+type (
+	// ClientInfoHandler get client info from request
+	ClientInfoHandler func(c *fiber.Ctx) (clientID, clientSecret string, err error)
+
+	// ClientAuthorizedHandler check the client allows to use this authorization grant type
+	ClientAuthorizedHandler func(clientID string, grant oauth2.GrantType) (allowed bool, err error)
+
+	// ClientScopeHandler check the client allows to use scope
+	ClientScopeHandler func(tgr *oauth2.TokenGenerateRequest) (allowed bool, err error)
+
+	// UserAuthorizationHandler get user id from request authorization
+	UserAuthorizationHandler func(c *fiber.Ctx) (userID string, err error)
+
+	// PasswordAuthorizationHandler get user id from username and password
+	PasswordAuthorizationHandler func(username, password string) (userID string, err error)
+
+	// RefreshingScopeHandler check the scope of the refreshing token
+	RefreshingScopeHandler func(tgr *oauth2.TokenGenerateRequest, oldScope string) (allowed bool, err error)
+
+	// RefreshingValidationHandler check if refresh_token is still valid. eg no revocation or other
+	RefreshingValidationHandler func(ti oauth2.TokenInfo) (allowed bool, err error)
+
+	// ResponseErrorHandler response error handing
+	ResponseErrorHandler func(re *errors.Response)
+
+	// InternalErrorHandler internal error handing
+	InternalErrorHandler func(err error) (re *errors.Response)
+
+	// AuthorizeScopeHandler set the authorized scope
+	AuthorizeScopeHandler func(c *fiber.Ctx) (scope string, err error)
+
+	// AccessTokenExpHandler set expiration date for the access token
+	AccessTokenExpHandler func(c *fiber.Ctx) (exp time.Duration, err error)
+
+	// ExtensionFieldsHandler in response to the access token with the extension of the field
+	ExtensionFieldsHandler func(ti oauth2.TokenInfo) (fieldsValue map[string]interface{})
+
+	// ResponseTokenHandler response token handing
+	ResponseTokenHandler func(c *fiber.Ctx, data map[string]interface{}, header map[string][]string, statusCode ...int) error
+)
+
+// ClientFormHandler get client data from form
+func ClientFormHandler(c *fiber.Ctx) (string, string, error) {
+	clientID := c.FormValue("client_id")
+	if clientID == "" {
+		return "", "", errors.ErrInvalidClient
+	}
+	clientSecret := c.FormValue("client_secret")
+	return clientID, clientSecret, nil
+}
+
+func ClientBasicHandler(c *fiber.Ctx) (string, string, error) {
+	if auth := c.Get("Authorization"); auth != "" {
+		if username, password, ok := parseBasicAuth(auth); ok {
+			return username, password, nil
+		}
+	}
+	return "", "", errors.ErrInvalidClient
+}
+
+// parseBasicAuth parses an HTTP Basic Authentication string.
+// "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==" returns ("Aladdin", "open sesame", true).
+func parseBasicAuth(auth string) (username, password string, ok bool) {
+	const prefix = "Basic "
+	// Case insensitive prefix match. See Issue 22736.
+	if len(auth) < len(prefix) || !EqualFold(auth[:len(prefix)], prefix) {
+		return
+	}
+	c, err := base64.StdEncoding.DecodeString(auth[len(prefix):])
+	if err != nil {
+		return
+	}
+	cs := string(c)
+	s := strings.IndexByte(cs, ':')
+	if s < 0 {
+		return
+	}
+	return cs[:s], cs[s+1:], true
+}
+
+// EqualFold is strings.EqualFold, ASCII only. It reports whether s and t
+// are equal, ASCII-case-insensitively.
+func EqualFold(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	for i := 0; i < len(s); i++ {
+		if lower(s[i]) != lower(t[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// lower returns the ASCII lowercase version of b.
+func lower(b byte) byte {
+	if 'A' <= b && b <= 'Z' {
+		return b + ('a' - 'A')
+	}
+	return b
+}
---
+++ server.go
@@ -0,0 +1,588 @@
+package oauth2fiber
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/gofiber/fiber/v2"
+)
+
+// NewDefaultServer create a default authorization server
+func NewDefaultServer(manager oauth2.Manager) *Server {
+	return NewServer(NewConfig(), manager)
+}
+
+// NewServer create authorization server
+func NewServer(cfg *Config, manager oauth2.Manager) *Server {
+	srv := &Server{
+		Config:  cfg,
+		Manager: manager,
+	}
+
+	// default handler
+	srv.ClientInfoHandler = ClientBasicHandler
+
+	srv.UserAuthorizationHandler = func(c *fiber.Ctx) (string, error) {
+		return "", errors.ErrAccessDenied
+	}
+
+	srv.PasswordAuthorizationHandler = func(username, password string) (string, error) {
+		return "", errors.ErrAccessDenied
+	}
+	return srv
+}
+
+// Server Provide authorization server
+type Server struct {
+	Config                       *Config
+	Manager                      oauth2.Manager
+	ClientInfoHandler            ClientInfoHandler
+	ClientAuthorizedHandler      ClientAuthorizedHandler
+	ClientScopeHandler           ClientScopeHandler
+	UserAuthorizationHandler     UserAuthorizationHandler
+	PasswordAuthorizationHandler PasswordAuthorizationHandler
+	RefreshingValidationHandler  RefreshingValidationHandler
+	RefreshingScopeHandler       RefreshingScopeHandler
+	ResponseErrorHandler         ResponseErrorHandler
+	InternalErrorHandler         InternalErrorHandler
+	ExtensionFieldsHandler       ExtensionFieldsHandler
+	AccessTokenExpHandler        AccessTokenExpHandler
+	AuthorizeScopeHandler        AuthorizeScopeHandler
+	ResponseTokenHandler         ResponseTokenHandler
+}
+
+func (s *Server) redirectError(c *fiber.Ctx, req *AuthorizeRequest, err error) error {
+	if req == nil {
+		return err
+	}
+	data, _, _ := s.GetErrorData(err)
+	return s.redirect(c, req, data)
+}
+
+func (s *Server) redirect(c *fiber.Ctx, req *AuthorizeRequest, data map[string]interface{}) error {
+	uri, err := s.GetRedirectURI(req, data)
+	if err != nil {
+		return err
+	}
+
+	c.Set("Location", uri)
+	c.Status(302)
+	return nil
+}
+
+func (s *Server) tokenError(c *fiber.Ctx, err error) error {
+	data, statusCode, header := s.GetErrorData(err)
+	return s.token(c, data, header, statusCode)
+}
+
+func (s *Server) token(c *fiber.Ctx, data map[string]interface{}, header map[string][]string, statusCode ...int) error {
+	if fn := s.ResponseTokenHandler; fn != nil {
+		return fn(c, data, header, statusCode...)
+	}
+	c.Set("Content-Type", "application/json;charset=UTF-8")
+	c.Set("Cache-Control", "no-store")
+	c.Set("Pragma", "no-cache")
+
+	for k, v := range header {
+		for _, vv := range v {
+			c.Set(k, vv)
+		}
+	}
+
+	status := fiber.StatusOK
+	if len(statusCode) > 0 && statusCode[0] > 0 {
+		status = statusCode[0]
+	}
+
+	c.Status(status)
+	return c.JSON(data)
+}
+
+// GetRedirectURI get redirect uri
+func (s *Server) GetRedirectURI(req *AuthorizeRequest, data map[string]interface{}) (string, error) {
+	u, err := url.Parse(req.RedirectURI)
+	if err != nil {
+		return "", err
+	}
+
+	q := u.Query()
+	if req.State != "" {
+		q.Set("state", req.State)
+	}
+
+	for k, v := range data {
+		q.Set(k, fmt.Sprint(v))
+	}
+
+	switch req.ResponseType {
+	case oauth2.Code:
+		u.RawQuery = q.Encode()
+	case oauth2.Token:
+		u.RawQuery = ""
+		fragment, err := url.QueryUnescape(q.Encode())
+		if err != nil {
+			return "", err
+		}
+		u.Fragment = fragment
+	}
+
+	return u.String(), nil
+}
+
+// CheckResponseType check allows response type
+func (s *Server) CheckResponseType(rt oauth2.ResponseType) bool {
+	for _, art := range s.Config.AllowedResponseTypes {
+		if art == rt {
+			return true
+		}
+	}
+	return false
+}
+
+// CheckCodeChallengeMethod checks for allowed code challenge method
+func (s *Server) CheckCodeChallengeMethod(ccm oauth2.CodeChallengeMethod) bool {
+	for _, c := range s.Config.AllowedCodeChallengeMethods {
+		if c == ccm {
+			return true
+		}
+	}
+	return false
+}
+
+// ValidationAuthorizeRequest the authorization request validation
+func (s *Server) ValidationAuthorizeRequest(c *fiber.Ctx) (*AuthorizeRequest, error) {
+	redirectURI := c.FormValue("redirect_uri")
+	clientID := c.FormValue("client_id")
+	if !(c.Method() == "GET" || c.Method() == "POST") ||
+		clientID == "" {
+		return nil, errors.ErrInvalidRequest
+	}
+
+	resType := oauth2.ResponseType(c.FormValue("response_type"))
+	if resType.String() == "" {
+		return nil, errors.ErrUnsupportedResponseType
+	} else if allowed := s.CheckResponseType(resType); !allowed {
+		return nil, errors.ErrUnauthorizedClient
+	}
+
+	cc := c.FormValue("code_challenge")
+	if cc == "" && s.Config.ForcePKCE {
+		return nil, errors.ErrCodeChallengeRquired
+	}
+	if cc != "" && (len(cc) < 43 || len(cc) > 128) {
+		return nil, errors.ErrInvalidCodeChallengeLen
+	}
+
+	ccm := oauth2.CodeChallengeMethod(c.FormValue("code_challenge_method"))
+	// set default
+	if ccm == "" {
+		ccm = oauth2.CodeChallengePlain
+	}
+	if ccm.String() != "" && !s.CheckCodeChallengeMethod(ccm) {
+		return nil, errors.ErrUnsupportedCodeChallengeMethod
+	}
+
+	req := &AuthorizeRequest{
+		RedirectURI:         redirectURI,
+		ResponseType:        resType,
+		ClientID:            clientID,
+		State:               c.FormValue("state"),
+		Scope:               c.FormValue("scope"),
+		Ctx:                 c,
+		CodeChallenge:       cc,
+		CodeChallengeMethod: ccm,
+	}
+	return req, nil
+}
+
+// GetAuthorizeToken get authorization token(code)
+func (s *Server) GetAuthorizeToken(ctx context.Context, req *AuthorizeRequest) (oauth2.TokenInfo, error) {
+	// check the client allows the grant type
+	if fn := s.ClientAuthorizedHandler; fn != nil {
+		gt := oauth2.AuthorizationCode
+		if req.ResponseType == oauth2.Token {
+			gt = oauth2.Implicit
+		}
+
+		allowed, err := fn(req.ClientID, gt)
+		if err != nil {
+			return nil, err
+		} else if !allowed {
+			return nil, errors.ErrUnauthorizedClient
+		}
+	}
+
+	tgr := &oauth2.TokenGenerateRequest{
+		ClientID:       req.ClientID,
+		UserID:         req.UserID,
+		RedirectURI:    req.RedirectURI,
+		Scope:          req.Scope,
+		AccessTokenExp: req.AccessTokenExp,
+		// Request: req.Request,
+	}
+
+	// check the client allows the authorized scope
+	if fn := s.ClientScopeHandler; fn != nil {
+		allowed, err := fn(tgr)
+		if err != nil {
+			return nil, err
+		} else if !allowed {
+			return nil, errors.ErrInvalidScope
+		}
+	}
+
+	tgr.CodeChallenge = req.CodeChallenge
+	tgr.CodeChallengeMethod = req.CodeChallengeMethod
+
+	return s.Manager.GenerateAuthToken(ctx, req.ResponseType, tgr)
+}
+
+// GetAuthorizeData get authorization response data
+func (s *Server) GetAuthorizeData(rt oauth2.ResponseType, ti oauth2.TokenInfo) map[string]interface{} {
+	if rt == oauth2.Code {
+		return map[string]interface{}{
+			"code": ti.GetCode(),
+		}
+	}
+	return s.GetTokenData(ti)
+}
+
+// HandleAuthorizeRequest the authorization request handling
+func (s *Server) HandleAuthorizeRequest(c *fiber.Ctx) error {
+	ctx := c.Context()
+
+	req, err := s.ValidationAuthorizeRequest(c)
+	if err != nil {
+		return s.redirectError(c, req, err)
+	}
+
+	// user authorization
+	userID, err := s.UserAuthorizationHandler(c)
+	if err != nil {
+		return s.redirectError(c, req, err)
+	} else if userID == "" {
+		return nil
+	}
+	req.UserID = userID
+
+	// specify the scope of authorization
+	if fn := s.AuthorizeScopeHandler; fn != nil {
+		scope, err := fn(c)
+		if err != nil {
+			return err
+		} else if scope != "" {
+			req.Scope = scope
+		}
+	}
+
+	// specify the expiration time of access token
+	if fn := s.AccessTokenExpHandler; fn != nil {
+		exp, err := fn(c)
+		if err != nil {
+			return err
+		}
+		req.AccessTokenExp = exp
+	}
+
+	ti, err := s.GetAuthorizeToken(ctx, req)
+	if err != nil {
+		return s.redirectError(c, req, err)
+	}
+
+	// If the redirect URI is empty, the default domain provided by the client is used.
+	if req.RedirectURI == "" {
+		client, err := s.Manager.GetClient(ctx, req.ClientID)
+		if err != nil {
+			return err
+		}
+		req.RedirectURI = client.GetDomain()
+	}
+
+	return s.redirect(c, req, s.GetAuthorizeData(req.ResponseType, ti))
+}
+
+// ValidationTokenRequest the token request validation
+func (s *Server) ValidationTokenRequest(c *fiber.Ctx) (oauth2.GrantType, *oauth2.TokenGenerateRequest, error) {
+	if v := c.Method(); !(v == "POST" ||
+		(s.Config.AllowGetAccessRequest && v == "GET")) {
+		return "", nil, errors.ErrInvalidRequest
+	}
+
+	gt := oauth2.GrantType(c.FormValue("grant_type"))
+	if gt.String() == "" {
+		return "", nil, errors.ErrUnsupportedGrantType
+	}
+
+	clientID, clientSecret, err := s.ClientInfoHandler(c)
+	if err != nil {
+		return "", nil, err
+	}
+
+	tgr := &oauth2.TokenGenerateRequest{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		// Request:      r,
+	}
+
+	switch gt {
+	case oauth2.AuthorizationCode:
+		tgr.RedirectURI = c.FormValue("redirect_uri")
+		tgr.Code = c.FormValue("code")
+		if tgr.RedirectURI == "" ||
+			tgr.Code == "" {
+			return "", nil, errors.ErrInvalidRequest
+		}
+		tgr.CodeVerifier = c.FormValue("code_verifier")
+		if s.Config.ForcePKCE && tgr.CodeVerifier == "" {
+			return "", nil, errors.ErrInvalidRequest
+		}
+	case oauth2.PasswordCredentials:
+		tgr.Scope = c.FormValue("scope")
+		username, password := c.FormValue("username"), c.FormValue("password")
+		if username == "" || password == "" {
+			return "", nil, errors.ErrInvalidRequest
+		}
+
+		userID, err := s.PasswordAuthorizationHandler(username, password)
+		if err != nil {
+			return "", nil, err
+		} else if userID == "" {
+			return "", nil, errors.ErrInvalidGrant
+		}
+		tgr.UserID = userID
+	case oauth2.ClientCredentials:
+		tgr.Scope = c.FormValue("scope")
+	case oauth2.Refreshing:
+		tgr.Refresh = c.FormValue("refresh_token")
+		tgr.Scope = c.FormValue("scope")
+		if tgr.Refresh == "" {
+			return "", nil, errors.ErrInvalidRequest
+		}
+	}
+	return gt, tgr, nil
+}
+
+// CheckGrantType check allows grant type
+func (s *Server) CheckGrantType(gt oauth2.GrantType) bool {
+	for _, agt := range s.Config.AllowedGrantTypes {
+		if agt == gt {
+			return true
+		}
+	}
+	return false
+}
+
+// GetAccessToken access token
+func (s *Server) GetAccessToken(ctx context.Context, gt oauth2.GrantType, tgr *oauth2.TokenGenerateRequest) (oauth2.TokenInfo, error) {
+	if allowed := s.CheckGrantType(gt); !allowed {
+		return nil, errors.ErrUnauthorizedClient
+	}
+
+	if fn := s.ClientAuthorizedHandler; fn != nil {
+		allowed, err := fn(tgr.ClientID, gt)
+		if err != nil {
+			return nil, err
+		} else if !allowed {
+			return nil, errors.ErrUnauthorizedClient
+		}
+	}
+
+	switch gt {
+	case oauth2.AuthorizationCode:
+		ti, err := s.Manager.GenerateAccessToken(ctx, gt, tgr)
+		if err != nil {
+			switch err {
+			case errors.ErrInvalidAuthorizeCode, errors.ErrInvalidCodeChallenge, errors.ErrMissingCodeChallenge:
+				return nil, errors.ErrInvalidGrant
+			case errors.ErrInvalidClient:
+				return nil, errors.ErrInvalidClient
+			default:
+				return nil, err
+			}
+		}
+		return ti, nil
+	case oauth2.PasswordCredentials, oauth2.ClientCredentials:
+		if fn := s.ClientScopeHandler; fn != nil {
+			allowed, err := fn(tgr)
+			if err != nil {
+				return nil, err
+			} else if !allowed {
+				return nil, errors.ErrInvalidScope
+			}
+		}
+		return s.Manager.GenerateAccessToken(ctx, gt, tgr)
+	case oauth2.Refreshing:
+		// check scope
+		if scopeFn := s.RefreshingScopeHandler; tgr.Scope != "" && scopeFn != nil {
+			rti, err := s.Manager.LoadRefreshToken(ctx, tgr.Refresh)
+			if err != nil {
+				if err == errors.ErrInvalidRefreshToken || err == errors.ErrExpiredRefreshToken {
+					return nil, errors.ErrInvalidGrant
+				}
+				return nil, err
+			}
+
+			allowed, err := scopeFn(tgr, rti.GetScope())
+			if err != nil {
+				return nil, err
+			} else if !allowed {
+				return nil, errors.ErrInvalidScope
+			}
+		}
+
+		if validationFn := s.RefreshingValidationHandler; validationFn != nil {
+			rti, err := s.Manager.LoadRefreshToken(ctx, tgr.Refresh)
+			if err != nil {
+				if err == errors.ErrInvalidRefreshToken || err == errors.ErrExpiredRefreshToken {
+					return nil, errors.ErrInvalidGrant
+				}
+				return nil, err
+			}
+			allowed, err := validationFn(rti)
+			if err != nil {
+				return nil, err
+			} else if !allowed {
+				return nil, errors.ErrInvalidScope
+			}
+		}
+
+		ti, err := s.Manager.RefreshAccessToken(ctx, tgr)
+		if err != nil {
+			if err == errors.ErrInvalidRefreshToken || err == errors.ErrExpiredRefreshToken {
+				return nil, errors.ErrInvalidGrant
+			}
+			return nil, err
+		}
+		return ti, nil
+	}
+
+	return nil, errors.ErrUnsupportedGrantType
+}
+
+// GetTokenData token data
+func (s *Server) GetTokenData(ti oauth2.TokenInfo) map[string]interface{} {
+	data := map[string]interface{}{
+		"access_token": ti.GetAccess(),
+		"token_type":   s.Config.TokenType,
+		"expires_in":   int64(ti.GetAccessExpiresIn() / time.Second),
+	}
+
+	if scope := ti.GetScope(); scope != "" {
+		data["scope"] = scope
+	}
+
+	if refresh := ti.GetRefresh(); refresh != "" {
+		data["refresh_token"] = refresh
+	}
+
+	if fn := s.ExtensionFieldsHandler; fn != nil {
+		ext := fn(ti)
+		for k, v := range ext {
+			if _, ok := data[k]; ok {
+				continue
+			}
+			data[k] = v
+		}
+	}
+	return data
+}
+
+// HandleTokenRequest token request handling
+func (s *Server) HandleTokenRequest(c *fiber.Ctx) error {
+	ctx := c.Context()
+
+	gt, tgr, err := s.ValidationTokenRequest(c)
+	if err != nil {
+		return s.tokenError(c, err)
+	}
+
+	ti, err := s.GetAccessToken(ctx, gt, tgr)
+	if err != nil {
+		return s.tokenError(c, err)
+	}
+
+	return s.token(c, s.GetTokenData(ti), nil)
+}
+
+// GetErrorData get error response data
+func (s *Server) GetErrorData(err error) (data map[string]interface{}, statusCode int, header map[string][]string) {
+	var re errors.Response
+	if v, ok := errors.Descriptions[err]; ok {
+		re.Error = err
+		re.Description = v
+		re.StatusCode = errors.StatusCodes[err]
+	} else {
+		if fn := s.InternalErrorHandler; fn != nil {
+			if v := fn(err); v != nil {
+				re = *v
+			}
+		}
+
+		if re.Error == nil {
+			re.Error = errors.ErrServerError
+			re.Description = errors.Descriptions[errors.ErrServerError]
+			re.StatusCode = errors.StatusCodes[errors.ErrServerError]
+		}
+	}
+
+	if fn := s.ResponseErrorHandler; fn != nil {
+		go fn(&re)
+	}
+
+	data = make(map[string]interface{})
+	if err := re.Error; err != nil {
+		data["error"] = err.Error()
+	}
+
+	if v := re.ErrorCode; v != 0 {
+		data["error_code"] = v
+	}
+
+	if v := re.Description; v != "" {
+		data["error_description"] = v
+	}
+
+	if v := re.URI; v != "" {
+		data["error_uri"] = v
+	}
+
+	statusCode = fiber.StatusInternalServerError
+	if v := re.StatusCode; v > 0 {
+		statusCode = v
+	}
+
+	return data, statusCode, re.Header
+}
+
+// BearerAuth parse bearer token
+func (s *Server) BearerAuth(c *fiber.Ctx) (string, bool) {
+	auth := c.Get("Authorization")
+	prefix := "Bearer "
+	token := ""
+
+	if auth != "" && strings.HasPrefix(auth, prefix) {
+		token = auth[len(prefix):]
+	} else {
+		token = c.FormValue("access_token")
+	}
+
+	return token, token != ""
+}
+
+// ValidationBearerToken validation the bearer tokens
+// https://tools.ietf.org/html/rfc6750
+func (s *Server) ValidationBearerToken(c *fiber.Ctx) (oauth2.TokenInfo, error) {
+	ctx := c.Context()
+
+	accessToken, ok := s.BearerAuth(c)
+	if !ok {
+		return nil, errors.ErrInvalidAccessToken
+	}
+
+	return s.Manager.LoadAccessToken(ctx, accessToken)
+}
